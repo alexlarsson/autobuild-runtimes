1f0660ed76c44fdcb6d00427482ffc76b0c30939
Alexander Larsson <alexl@redhat.com>
2016-04-08 09:20:25 +0000

    Move build to build.sh
    run_nightly.sh just starts build.sh

--- run_nightly.sh
+++ run_nightly.sh
@@ -4,53 +4,5 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd $SCRIPTPATH
+./build.sh
 
-source ./envs.sh
-
-mkdir -p gnupg export/logs export/keys
-
-function init_repo {
-    if [ ! -d  export/repo-$1  ] ; then
-	ostree  init --mode=archive-z2 --repo=export/repo-$1
-    fi
-}
-init_repo staging
-init_repo staging-apps
-init_repo staging-nightly
-init_repo staging-nightly-apps
-
-T=`date +%Y-%m-%d-%H:%M`
-echo Build Freedesktop base at $T
-./build_base_runtime.sh org.freedesktop 1.4 git://anongit.freedesktop.org/xdg-app/freedesktop-sdk-base 1.4 repo-staging &> export/logs/build-fdo-base-1.4-$T.log
-
-echo Build Freedesktop 1.4
-T=`date +%Y-%m-%d-%H:%M`
-./build_runtime.sh org.freedesktop 1.4 git://anongit.freedesktop.org/xdg-app/freedesktop-sdk-images master repo-staging &> export/logs/build-fdo-1.4-$T.log
-
-echo Build Gnome 3.20
-T=`date +%Y-%m-%d-%H:%M`
-./build_runtime.sh org.gnome 3.20 https://git.gnome.org/browse/gnome-sdk-images gnome-3-20 repo-staging &> export/logs/build-gnome-3.20-$T.log
-
-T=`date +%Y-%m-%d-%H:%M`
-echo Update Staging Repo
-../lock.sh ./update_repo.sh repo-staging -1 &> export/logs/update-repo-staging-$T.log
-
-echo Build Gnome Master
-T=`date +%Y-%m-%d-%H:%M`
-./build_runtime.sh org.gnome master https://git.gnome.org/browse/gnome-sdk-images master repo-nightly &> export/logs/build-gnome-master-$T.log
-
-T=`date +%Y-%m-%d-%H:%M`
-echo Update Nightly Repo
-../lock.sh ./update_repo.sh repo-nightly 4 &> export/logs/update-repo-nightly-$T.log
-
-echo Build Apps Gnome Master
-./build_apps.sh gnome-apps-nightly https://github.com/alexlarsson/gnome-apps-nightly.git master repo-nightly-apps &> export/logs/build-gnome-master-$T.log
-
-T=`date +%Y-%m-%d-%H:%M`
-echo Update Nightly Apps Repo
-../lock.sh ./update_repo.sh repo-nightly-apps 4 &> export/logs/update-repo-nightly-apps-$T.log
-
-# Remove old logs
-find export/logs -mtime +5 -exec rm {} \;
-
-echo DONE
