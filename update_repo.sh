8870d2a1f15e8ee27480c7e4f392f62a9c600b7e
Alexander Larsson <alexl@redhat.com>
2016-04-11 11:55:02 +0000

    Handle the no-deltas case

--- update_repo.sh
+++ update_repo.sh
@@ -5,16 +5,22 @@ PRUNE_DEPTH=$2
 
 REFS=$(for r in $(ostree refs --repo=export/$REPO) ; do echo $(ostree rev-parse --repo=export/$REPO $r ); done)
 
-# Only keep deltas to the refs heads
-for d in $(ostree static-delta list --repo=export/$REPO); do
-    t=$(echo $d | sed "s/.*-//")
-    if [[ $REFS =~ $t ]] ; then
-	echo keeping delta $d
-    else
-	echo remove delta $d
-	ostree static-delta delete  --repo=export/$REPO $d
-    fi
-done
+if ostree static-delta list --repo=export/repo-staging | grep "No static" ; then
+    # No static deltas to remove
+    true
+else
+    # Only keep deltas to the refs heads
+    ostree static-delta list --repo=export/$REPO
+    for d in $(ostree static-delta list --repo=export/$REPO); do
+	t=$(echo $d | sed "s/.*-//")
+	if [[ $REFS =~ $t ]] ; then
+	    echo keeping delta $d
+	else
+	    echo remove delta $d
+	    ostree static-delta delete  --repo=export/$REPO $d
+	fi
+    done
+fi
 
 xdg-app build-update-repo --generate-static-deltas --prune --prune-depth=$PRUNE_DEPTH  ${EXPORT_ARGS-} export/$REPO
 
